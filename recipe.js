280943e3ec853fbdac51f0610119bf2a8a20da94
authorbeard <msnolan@gmail.com>
2016-04-25 08:32:36 -0400

    all specs passing

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function addIngredient(recipe, ingName, amts) {
+  recipe[ingName]=amts;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingName) {
+  delete recipe[ingName];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingName, amt) {
+  recipe[ingName]=amt;
+  return recipe;
+}
+
+function readRecipe(recipe) {
+  for (var key in recipe) {
+    console.log("this recipe calls for " + recipe[key] + " of " + key);
+  };
+}
